1778add233031e96cf4298c5f7eb61ee4b73d6cf
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-11-28 23:34:08 +0000

    Added inference engine imports and files

---
+++ ICA2-TeamM/src/TownTraversal.clj
@@ -0,0 +1,6 @@
+(
+  ns TownTraversal
+  (load-file "src/Inference Engines/Astar-search(2a).clj")
+  (load-file "src/Inference Engines/planner(1a).clj")
+  (load-file "src/Inference Engines/breadth-search(2a).clj")
+  )
