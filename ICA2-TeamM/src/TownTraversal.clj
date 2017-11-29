d24c98e24a15ca346fc179d2a6600acb90336450
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-11-29 00:02:34 +0000

    Updated clj with working breadth first and planner ops

--- ICA2-TeamM/src/TownTraversal.clj
+++ ICA2-TeamM/src/TownTraversal.clj
@@ -5,103 +5,25 @@
   (load-file "src/Inference Engines/breadth-search.clj")
   )
 
-;;A map of town nodes to locations they're connected to and the costs to travel those connections
-(def town-map-keyd
+;;Planner
+
+(def ops
   '{
-    :primary-school
-    {
-     :bakery {:cost 10}
-     }
-    :gregs-house
-    {
-     :bakery {:cost 5}
-     }
-    :bakery
-    {
-     :gregs-house {:cost 5}
-     :butchers {:cost 1}
-     :flower-shop {:cost 6}
-     :primary-school {:cost 10}
-     }
-    :flower-shop
-    {
-     :bakery {:cost 6}
-     :chinese-takeaway {:cost 8}
-     :hospital {:cost 10}
-     }
-    :hospital
-    {
-     :flower-shop {:cost 10}
-     }
-    :butchers
-    {
-     :bakery {:cost 1}
-     :chinese-takeaway {:cost 9}
-     :town-centre {:cost 5}
-     }
-    :chinese-takeaway
-    {
-     :flower-shop {:cost 8}
-     :butchers {:cost 9}
-     :the-club {:cost 6}
-     }
-    :secondary-school
-    {
-     :town-centre {:cost 1}
-     :supermarket {:cost 9}
-     }
-    :town-centre
-    {
-     :butchers {:cost 5}
-     :burger-town {:cost 10}
-     :fire-station {:cost 3}
-     :secondary-school {:cost 1}
-     :supermarket {:cost 8}
-     }
-    :the-club
-    {
-     :chinese-takeaway {:cost 6}
-     :train-station {:cost 8}
-     :police-station {:cost 2}
-     }
-    :supermarket
-    {
-     :secondary-school {:cost 9}
-     :town-centre {:cost 8}
-     :dock {:cost 8}
-     }
-    :burger-town
-    {
-     :town-centre {:cost 10}
-     :gym {:cost 9}
-     }
-    :dock
-    {
-     :supermarket {:cost 8}
-     :church {:cost 1}
-     }
-    :church
-    {
-     :dock {:cost 1}
-     :fire-station {:cost 7}
-     }
-    :fire-station
-    {
-     :church {:cost 7}
-     :gym {:cost 9}
-     }
-    :gym
-    {
-     :fire-station {:cost 9}
-     :burger-town {:cost 9}
-     :police-station {:cost 2}
-     }
-    :police-station
-    {
-     :gym {:cost 2}
-     :the-club {:cost 2}
-     }
-    })
+    :name move
+    :achieves (at ?agent ?y)
+    :pre ((agent ?agent) (at ?agent ?x) (connected ?x ?y))
+    :add ((at ?agent y))
+    :del ((at ?agent x))
+    :txt ((moved from ?x to ?y))
+    :cmd [move-to ?y]
+    }
+  )
+
+;; Need world states described to it
+
+;; A-Star
+
+;; Breadth first (Currently doesn't take into account cost , will see if possible)
 
 (def town-map-keyless-costless
   '{
@@ -200,8 +122,4 @@
      }
     })
 
-;;A-Star
-
-;;Planner
-
-;;Breadth first
+(breadth-search 'police-station 'gym town-map-keyless-costless)
